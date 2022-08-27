2f42ef0d61f67662fc6cbcf88c2ff76a07bab145
Frank Lemanschik <frank@lemanschik.com>
2022-08-27 13:49:21 +0200

    Update git-subrepo-cli.js
    lisp like version that looks promissing.

--- git-subrepo-cli.js
+++ git-subrepo-cli.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 
-require('node:child_process').exec(__dirname+'/lib/git-repo'
-                                  // [, options][, callback]
-                                  )
+require('node:child_process').exec(__dirname+'/lib/git-subrepo', { stdio: "inhire"},
+(error, _stdout, _stderr) => (error) && console.error(`exec error: ${error}`));
