10c0a89153f8e780129293a67b5e604fe256eb16
Frank Lemanschik <frank@lemanschik.com>
2022-08-29 05:50:50 +0200

    Delete git-subrepo-cli.js
    CLI parsing in nodejs is a bad pattern 👍🏻

--- git-subrepo-cli.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-require('node:child_process').exec(__dirname+'/lib/git-subrepo '+process.argv.slice(1).join(' '), { stdio: "inhire"},
-(error, _stdout, _stderr) => (error) && console.error(`exec error: ${error}`));
