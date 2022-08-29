a099aebbae9084c98251a2d7e14b8ab7792d1d0f
Frank Lemanschik <frank@lemanschik.com>
2022-08-29 03:07:43 +0200

    Update git-subrepo-cli.js
    ups not converted , to space

--- git-subrepo-cli.js
+++ git-subrepo-cli.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
-require('node:child_process').exec(__dirname+'/lib/git-subrepo '+process.argv.slice(1), { stdio: "inhire"},
+require('node:child_process').exec(__dirname+'/lib/git-subrepo '+process.argv.slice(1).join(' '), { stdio: "inhire"},
 (error, _stdout, _stderr) => (error) && console.error(`exec error: ${error}`));
